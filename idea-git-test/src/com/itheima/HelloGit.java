87d4237de892bc9395ee0cf699d4bc5c06ac5a72
admin <admin@itcast.cn>
2019-02-20 20:11:20 +0800

    ffffff

---
+++ idea-git-test/src/com/itheima/HelloGit.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * @author Alice
+ * @date 2019/2/20
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
